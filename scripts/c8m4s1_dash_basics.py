779f0a6e55cee39a8a77220d424e6984b2b3747f
HrushiSanap <hrushisanap@outlook.com>
2025-05-03 17:25:51 +0100

    course 8 module 4
    this commit includes new folder named "scripts" which includes python scripts to create web-applications using dash.

---
+++ scripts/c8m4s1_dash_basics.py
@@ -0,0 +1,48 @@
+#import libraries
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+
+# Read the airline data into pandas dataframe
+df = pd.read_csv("datasets/airline.csv",
+                 encoding = "ISO-8859-1",
+                 dtype= {'Div1Airport': str, 'Div1TailNum': str, 
+                        'Div2Airport': str, 'Div2TailNum': str}
+                )
+
+# Randomly sample 500 data points. Setting the random state to be 42 so that we get same result.
+data = df.sample(n = 500, random_state=42)
+
+# Pie Chart Creation
+fig = px.pie(data, values  = "Flights", names = "DistanceGroup",
+             title  = "Distance Group proportion by Flights")
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+
+# Get the layout of the application and adjust it.
+# Create an outer division using html.Div and add title to the dashboard using html.H1 component
+# Add description about the graph using HTML P (paragraph) component
+# Finally, add graph component.
+app.layout = html.Div(children=[html.H1("Airline Dashboard",
+                                        style = {"textAlign": "center",
+                                                 "color": "#503D36",
+                                                 "font-size":40
+                                                 }
+                                        ),
+                                html.P('Proportion of distance group (250 mile distance interval group) by flights.',
+                                       style = {"textAlign":"center",
+                                                "color":'#F57241'
+                                                }
+                                        ),
+                                dcc.Graph(figure = fig)
+                                ]
+                        )
+
+
+# Run the application                   
+if __name__ == "__main__":
+    app.run()
